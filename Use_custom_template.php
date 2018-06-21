8c6ebaed45153bea5dd3eb6e32326ee331df39ae
Mohammad Reza <rza9001@gmail.com>
2018-06-21 08:38:13 +0700

    Added Use_custom_template.php

---
+++ Use_custom_template.php
@@ -0,0 +1,4 @@
+ <!-- Folder templates, nama file reservation-form.php -->
+ <?php get_template_part('templates/reservation', 'form'); ?>
+
+ <?php get_template_part('nama folder/nama file', 'sambungan nama file'); ?> 
