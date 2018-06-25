02e5ee4d278dab3f78635bc50faa47a4452dcc8a
Mohammad Reza <rza9001@gmail.com>
2018-06-25 16:28:49 +0700

    Added Load_logo.php

---
+++ Load_logo.php
@@ -0,0 +1,10 @@
+  <header id="masthead" class="site-header" role="banner">
+    <div class="container">
+      <div class="logo">
+        <a href="<?php echo esc_url(home_url('/')); ?>">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo">
+        </a>  
+      </div>
+    </div>
+    <!-- Container -->
+  </header>
