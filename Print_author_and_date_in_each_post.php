1fa56c1f3f2661f4d890dd6a5aadc39f6f104035
Mohammad Reza <rza9001@gmail.com>
2018-06-29 14:22:22 +0700

    Added Print_author_and_date_in_each_post.php

---
+++ Print_author_and_date_in_each_post.php
@@ -0,0 +1,10 @@
+<?php    
+    <div class="post-information">
+      <div class="author">
+        By: <?php the_author(); ?>
+      </div>
+
+      <div class="date">
+        <?php the_time('F j, Y'); ?>
+      </div>
+    </div>
