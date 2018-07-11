5c8c3b272ced9dcd1ecbefbea913debf577b3fc5
Mohammad Reza <rza9001@gmail.com>
2018-07-11 20:17:50 +0700

    Added Get_template_part.php

---
+++ Get_template_part.php
@@ -0,0 +1,9 @@
+<?php
+
+// Buat folder namanya: template-parts
+// Buat file php isinya section2 dari page2
+
+// load file template-parts ke page.php
+<?php get_template_part('template-parts/nama_file (gak pake ext)') ?>
+
+?>
