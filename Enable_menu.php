49dda83bf2db3624c5382637b76351523cf7dfa7
Mohammad Reza <rza9001@gmail.com>
2018-07-29 18:02:48 +0700

    Added Enable_menu.php

--- Enable_menu.php
+++ Enable_menu.php
@@ -31,7 +31,9 @@ add_action('init', 'lapizzeria_menus');
 <!-- functions.php -->
   // Enable Menu
   register_nav_menus(array(
-    'main_menu' => __('Main Menu', 'thetravelblog')
+    'main_menu' => __('Main Menu', 'thetravelblog'),
+    <!-- Social icon menu -->
+    'social_menu' => __('Social Menu', 'thetravelblog')
   ));
 
 <!-- header.php -->
@@ -45,3 +47,55 @@ add_action('init', 'lapizzeria_menus');
         'menu_class' => 'nav navbar-nav navbar-right'
       )); ?>
     </div>
+
+    <!-- Footer.php -->
+    <div class="col-sm-4">
+      <!-- Enable menu social icon-->
+      <?php
+         wp_nav_menu(array(
+                'theme_location' => 'social_menu',
+                'container' => 'div',
+                // Menambah ID untuk Div
+                'container_id' => 'menu-social',
+                // Menambah class untuk Div
+                'container_class' => 'menu',
+                 // Menambah ID untuk ul
+                'menu_id' => 'social',
+                // Menambah class untuk ul
+                'menu_class' => 'menu-items',
+                'depth' => 1,
+                'link_before' => '<span class="sr-only">',
+                'link_after' => '</span>',
+                'fallback_cb' => '',
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>'
+              ));
+      ?>
+    </div>
+
+
+    <!-- style.css -->
+    /* Style social icon */
+#social {
+  list-style: none;
+}
+
+#social li {
+  position: relative;
+  display: inline-block;
+}
+
+#social li a:before {
+  display: inline-block;
+  vertical-align: top;
+  padding: 0 5px;
+  font-family: 'fontAwesome';
+  font-size: 40px;
+  color: #fff;
+  content: '\f08e';
+  -webkit-font-smoothing: antialiased;
+}
+
+#social li a[href*="facebook.com"]::before{content:'\f082';}
+#social li a[href*="plus.google.com"]::before{content:'\f0d4';}
+#social li a[href*="twitter.com"]::before{content:'\f081';}
+#social li a[href*="youtube.com"]::before{content:'\f16a';}
