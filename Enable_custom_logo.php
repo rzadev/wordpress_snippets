3a02762400141bd8a2b351f47f11cc47ca1b2449
Mohammad Reza <rza9001@gmail.com>
2018-09-14 22:37:47 +0700

    Added Enable_custom_logo.php

--- Enable_custom_logo.php
+++ Enable_custom_logo.php
@@ -1,13 +1,13 @@
 <?php
 // <!-- functions.php -->
 
+
+// Enable custom logo
 function name_custom_logo() {
   $logo = array(
-    'height' => 200,
-    'width' => 250
+    'width' => 250,
+    'height' => 200
   );
-
-   // Enable custom logo
    add_theme_support('custom-logo', $logo);
 }
 add_action('after_setup_theme', 'name_custom_logo');
@@ -15,6 +15,14 @@ add_action('after_setup_theme', 'name_custom_logo');
 
 
 // <!-- header.php -->
-if(function_exists('the_custom_logo')) {
-  the_custom_logo();
-}
+<a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
+  // Print Logo 
+	if(function_exists('the_custom_logo')) {
+	  the_custom_logo();
+	}
+  // Hard code logo
+	// <img src="<?php echo get_template_directory_uri() ?>/images/logo.png" alt="Logo" class="logo"> -> delete hard 
+</a>
+
+
+<!-- Edit the css  -->
